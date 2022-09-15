3ee79e3db768f1741418eb6dcfc9add480dca1c1
Lee Hyung Won <38280577+ijhwlee@users.noreply.github.com>
2022-09-16 03:02:15 +0900

    Add files via upload
    404 문장 수정

--- y2022/Ch03/00-meadowlark.js
+++ y2022/Ch03/00-meadowlark.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.use((req, res)=>{
+app.use((req, res) => {
     res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    res.send('404 - File Not Found')
 })
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     console.error(err.message)
     res.type('text/plain')
     res.status(500)
     res.send('500 - Server Error')
 })
-
-app.listen(port, ()=> console.log(
-    `Express started on http://localhost:${port}; ` +
-    `press Ctrl-C to terminate.`
+app.listen(port, ()=>console.log(
+    `Express started on http://localhost:${port}; `+
+    `press Ctrl-C to terminate...`
 ))
