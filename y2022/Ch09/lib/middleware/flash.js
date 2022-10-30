0c50d468bbe389116006bd25110b074bb72cfb82
Lee Hyung Won <38280577+ijhwlee@users.noreply.github.com>
2022-10-31 01:48:44 +0900

    Add files via upload

---
+++ y2022/Ch09/lib/middleware/flash.js
@@ -0,0 +1,9 @@
+const flashMiddleware = (req, res, next) => {
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash
+	delete req.session.flash
+	next()
+}
+
+module.exports = flashMiddleware
