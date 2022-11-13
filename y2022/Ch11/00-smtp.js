ba7a69ccc0c9b92ec7f1c116081e2b1c36dcf9a8
ijhwlee <hwlee@inje.ac.kr>
2022-11-13 20:33:19 +0900

    add sendgrid mailer program

--- y2022/Ch11/00-smtp.js
+++ y2022/Ch11/00-smtp.js
@@ -14,7 +14,7 @@ async function go() {
   try {
     const result = await mailTransport.sendMail({
       from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
-      to: 'joecustomer@gmail.com',
+      to: 'ijhwlee@daum.net',
       subject: 'Your Meadowlark Travel Tour',
       text: 'Thank you for booking your trip with Meadowlark Travel.  ' +
         'We look forward to your visit!',
--- y2022/Ch11/credentials.js
+++ y2022/Ch11/credentials.js
@@ -1,7 +1,17 @@
 module.exports = {
     cookieSecret: '924c53cb-479e-42b5-b6b6-0f5d97cd3a07',
+    // sendgrid: {
+    //   user: 'apikey',
+    //   password: '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',
+    // },
     sendgrid: {
-      user: 'apikey',
-      password: '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',
+      user: 'web2',
+      api_key: 'Zj9sk5Y_Qwihj0W_GwW84A',
+      password: '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',
+    },
+    sendgrid2: {
+      user: 'MyMailAPI',
+      api_key: 'Zj9sk5Y_Qwihj0W_GwW84A',
+      password: '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',
     },
   }
