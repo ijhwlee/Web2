9420bd383e878534cb5e6c73aca5938d13c097c0
Lee Hyung Won <hwlee@inje.ac.kr>
2023-09-29 08:46:35 +0900

    Create 02-basic-rendering.js

---
+++ y2023/chap06/02-basic-rendering.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const expresshandlebars = require('express-handlebars')
+const app = express()
+const port = process.env.PORT || 3000
+
+//핸들바 뷰 엔진 설정
+app.engine('handlebars', expresshandlebars.engine({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', handlers.home)
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+// custom 404 page
+app.use(handlers.notFound)
+//custom 500 page
+app.use(handlers.serverError)
+
+app.listen(port, ()=> console.log(
+    `Express started on http://localhost:${port}; `+
+    `press Ctrl-C to terminate...`))
